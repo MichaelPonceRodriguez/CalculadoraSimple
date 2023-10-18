b7bb47e32ef897cceaceab02b6ab8021230170b5
MichaelPonce <michaelponce1012@gmail.com>
2023-10-18 12:28:15 -0300

    agregacion de funciones

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,26 @@
-function sumar(){
-    const formato = document.getElementById('formato');
+const formato = document.getElementById('formato');
     let numero1 = formato['operacion1'];
     let numero2 = formato['operacion2'];
-    let resultado = parseInt(numero1.value) + parseInt(numero2.value);
+    let resultado;
+
+function sumar(){
+    resultado = parseInt(numero1.value) + parseInt(numero2.value);
+    if(isNaN(resultado)) document.getElementById('resultado').innerHTML = 'Resultado: Debe contener numeros validos' ;
+    else document.getElementById('resultado').innerHTML = 'Resultado: ' + resultado;
+}
+
+function restar(){
+    resultado = parseInt(numero1.value) - parseInt(numero2.value);
+    if(isNaN(resultado)) document.getElementById('resultado').innerHTML = 'Resultado: Debe contener numeros validos' ;
+    else document.getElementById('resultado').innerHTML = 'Resultado: ' + resultado;
+}
+function dividir(){
+    resultado = parseInt(numero1.value) / parseInt(numero2.value);
+    if(isNaN(resultado)) document.getElementById('resultado').innerHTML = 'Resultado: Debe contener numeros validos' ;
+    else document.getElementById('resultado').innerHTML = 'Resultado: ' + resultado;
+}
+function multiplicar(){
+    resultado = parseInt(numero1.value) * parseInt(numero2.value);
     if(isNaN(resultado)) document.getElementById('resultado').innerHTML = 'Resultado: Debe contener numeros validos' ;
     else document.getElementById('resultado').innerHTML = 'Resultado: ' + resultado;
 }
